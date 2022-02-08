a755f1dc7e25c02c12cbcf7a38345d71a9320ac8
Aidan Macdonald <aidan.plenert.macdonald@gmail.com>
2022-02-08 15:21:19 +0200

    Conditionally create the sequence file

--- logstash/entrypoint.sh
+++ logstash/entrypoint.sh
@@ -1,6 +1,8 @@
-mkdir -p `dirname ${COUCHDB_SEQ}` \
-    && chown -R $USER.$USER `dirname ${COUCHDB_SEQ}` \
-    && echo 0 > ${COUCHDB_SEQ} \
-    && echo `dirname ${COUCHDB_SEQ}` \
-    && ls -lth `dirname ${COUCHDB_SEQ}` \
+if [[ -f "${COUCHDB_SEQ}" && -s "${COUCHDB_SEQ}" ]]; then
+    echo "Using existing sequence file"
+else
+    mkdir -p `dirname ${COUCHDB_SEQ}` \
+        && chown -R $USER.$USER `dirname ${COUCHDB_SEQ}` \
+        && echo 0 > ${COUCHDB_SEQ}
+fi \
     && /usr/local/bin/docker-entrypoint
