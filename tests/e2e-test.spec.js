db1f56840211720e22feed77f22c9663345733ae
Diana Barsan <barsan@medic.org>
2024-06-10 22:04:38 +0300

    use npm and latest node

--- tests/e2e-test.spec.js
+++ tests/e2e-test.spec.js
@@ -14,16 +14,19 @@ describe('Main workflow Test Suite', () => {
 
   it('should have data in postgres medic table', async () => {
     const couchdbTableResult = await client.query('SELECT * FROM ' + POSTGRES_SCHEMA + '.' + POSTGRES_TABLE);
+    console.log(couchdbTableResult.rows.length);
     expect(couchdbTableResult.rows.length).to.be.greaterThan(0);
   });
 
   it('should have data in postgres person table', async () => {
     const personTableResult = await client.query('SELECT * FROM ' + DBT_POSTGRES_SCHEMA + '.person');
+    console.log(personTableResult.rows.length);
     expect(personTableResult.rows.length).to.be.greaterThan(0);
   });
 
   it('should have data in postgres data_record table', async () => {
     const dataRecordTableResult = await client.query('SELECT * FROM ' + DBT_POSTGRES_SCHEMA + '.data_record');
+    console.log(dataRecordTableResult.rows.length);
     expect(dataRecordTableResult.rows.length).to.be.greaterThan(0);
   });
 });
