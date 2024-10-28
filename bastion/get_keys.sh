1f9a83d87378268dd1754bcad43c5607dc9f7bc4
mrjones-plip <mrjones-plip@plip.com>
2024-10-28 16:16:25 -0700

    give up on logging, update MOTD, try dymamic auth from GH keys

--- bastion/get_keys.sh
+++ bastion/get_keys.sh
@@ -2,5 +2,5 @@
 
 # todo 1 - get this to check github to see if $1 is in medic org and if they have an ssh key, fail if not
 # todo 2 - write this in node per Medic's requirements?
-echo $1 >> /tmp/logins
-echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKlJwZnBVjIpPgUu2GF34cPwUIFXapstbch8XfLn3rfR
+curl -qs https://github.com/mrjones-plip.keys
+curl -qs https://github.com/dianabarsan.keys
