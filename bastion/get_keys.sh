e94c01302d59976a5912e8f3fd570eea0eaf3a74
mrjones-plip <mrjones-plip@plip.com>
2024-11-15 17:29:07 -0800

    first pass at e2e tests

--- bastion/get_keys.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# todo:
-#  - have config pass in a GH org
-#  - get this to check github to see if $1 is in that  org and if they have an ssh key, fail if not
-#  - write this in node per Medic's requirements? would need to add node to the Dockerfile ... :/
---
+++ tests/utils/bastion-utils.js
@@ -0,0 +1,6 @@
+// todo - ;)
+
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(import.meta.dirname, '..', '.e2e-env') });
