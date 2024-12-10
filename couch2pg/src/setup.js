446438d4276838c6744a2f3a36c9a0cbce3ff5e9
Tom Wier <thomas@medic.org>
2024-12-10 14:19:58 +0300

    Revert "fix(#193): only create indexes if table does not exist"
    This reverts commit 99dc56348661b6ba34726fd5436b06a8fcf2ee2f.

--- couch2pg/src/setup.js
+++ couch2pg/src/setup.js
@@ -1,7 +1,6 @@
 import * as db from './db.js';
 
 const createSchema = `CREATE SCHEMA IF NOT EXISTS ${db.postgresSchema}`;
-
 const createTable = `
 CREATE TABLE IF NOT EXISTS ${db.postgresTable} (
   saved_timestamp TIMESTAMP,
@@ -26,37 +25,12 @@ const createTimestampIndex = `
 CREATE INDEX IF NOT EXISTS saved_timestamp ON ${db.postgresTable}(saved_timestamp);
 `;
 
-
-const checkTableExists = async (client) => {
-  const tableExists = `SELECT 1 FROM ${db.postgresTable} LIMIT 1;`;
-
-  try {
-    await client.query(tableExists);
-    return true;
-  } catch (error) {
-    // "Undefined table" error code in PostgreSQL
-    if (error.code === '42P01') {
-      return false;
-    }
-    throw error;
-  }
-};
-
 export const createDatabase = async () => {
   const client = await db.getPgClient();
   await client.query(createSchema);
-
-  // create the table and indexes if they don't exist
-  // note that `CREATE INDEX IF NOT EXISTS` acquires a lock
-  // even if the index exists, so cannot rely on it here,
-  // have to actually check if the table exists in a separate query
-  const tableExists = await checkTableExists(client);
-  if (!tableExists) {
-    await client.query(createTable);
-    await client.query(createDeleteIndex);
-    await client.query(createTimestampIndex);
-  }
-
+  await client.query(createTable);
+  await client.query(createDeleteIndex);
+  await client.query(createTimestampIndex);
   await client.query(createProgressTable);
   await client.end();
 };
